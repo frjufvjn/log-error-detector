6fb5881af44386337216cf98b6490e07411ee3da
Park Jeong Woo <frjufvjn@gmail.com>
2018-08-29 13:12:56 +0900

    arrange

--- logdetector.go
+++ logdetector.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-   	"bytes"
-   	"fmt"
-   	"log"
-   	"os"
-   	"strings"
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"strings"
 
-   	"github.com/fsnotify/fsnotify"
-   	"database/sql"
-   	_ "github.com/go-sql-driver/mysql"
+	"database/sql"
+
+	"github.com/fsnotify/fsnotify"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 const MYFILE = "/somepath/logfile.log"
@@ -20,74 +21,73 @@ var CONTROL = "" // make(chan string)
 
 func main() {
 
-   	watcher, err := fsnotify.NewWatcher()
+	watcher, err := fsnotify.NewWatcher()
 
-   	if err != nil {
-       	log.Fatal(err)
-   	}
+	if err != nil {
+		log.Fatal(err)
+	}
 
-   	defer watcher.Close()
+	defer watcher.Close()
 
-   	done := make(chan bool)
-   
-   	go func() {
-       	for 	{
-           	select {
-           	case event := <-watcher.Events:
-               // log.Println("event:", event)
+	done := make(chan bool)
 
-               if event.Op&fsnotify.Write == fsnotify.Write {
-                   // log.Println("-----------------------modified file:", event.Name)
-                    readFile(MYFILE)
-               	}
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				// log.Println("event:", event)
 
-           	case err := <-watcher.Errors:
-               log.Println("error:", err)
-			   
-           	}
-       	}
-   	}()
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					// log.Println("-----------------------modified file:", event.Name)
+					readFile(MYFILE)
+				}
 
-   	err = watcher.Add(MYFILE)
+			case err := <-watcher.Errors:
+				log.Println("error:", err)
 
-   	if err != nil {
-       	log.Fatal(err)
-   	}
-   	<-done
+			}
+		}
+	}()
+
+	err = watcher.Add(MYFILE)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
 
 }
 
-
 func readFile(fname string) {
 
-   	file, err := os.Open(fname)
+	file, err := os.Open(fname)
 
-   	if err != nil {
-       	panic(err)
-   	}
-   	defer file.Close()
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
 
-   	buf := make([]byte, READ_BUFFER_LIMIT)
-   	stat, err := os.Stat(fname)
-   	start := stat.Size() - READ_BUFFER_LIMIT
-   	_, err = file.ReadAt(buf, start)
+	buf := make([]byte, READ_BUFFER_LIMIT)
+	stat, err := os.Stat(fname)
+	start := stat.Size() - READ_BUFFER_LIMIT
+	_, err = file.ReadAt(buf, start)
 
-   	if err == nil {
-       	if bytes.Contains(buf, []byte(FIND_KEYWORD)) {
+	if err == nil {
+		if bytes.Contains(buf, []byte(FIND_KEYWORD)) {
 
-           	strBuf := string(buf[:])
-           	findStr := findKeywordUsingSplit(strBuf, FIND_KEYWORD)
-           	tmp := findStr[0:19]
+			strBuf := string(buf[:])
+			findStr := findKeywordUsingSplit(strBuf, FIND_KEYWORD)
+			tmp := findStr[0:19]
 
-           	if tmp != CONTROL {
-               	CONTROL = findStr[0:19]
-fmt.Println("start-----------------------------------------------")
-                           	fmt.Printf("%s\n", buf)
-fmt.Println("end-------------------------------------------------")
-               
+			if tmp != CONTROL {
+				CONTROL = findStr[0:19]
+				fmt.Println("start-----------------------------------------------")
+				fmt.Printf("%s\n", buf)
+				fmt.Println("end-------------------------------------------------")
+
 				db, err := sql.Open("mysql", "root:passwd@tcp(127.0.0.1:3306)/test")
 				if err != nil {
-                    log.Fatal(err)
+					log.Fatal(err)
 				}
 				defer db.Close()
 
@@ -103,40 +103,40 @@ fmt.Println("end-------------------------------------------------")
 }
 
 func dbCheckError(err error) {
-   if err != nil {
-       panic(err)
-   }
+	if err != nil {
+		panic(err)
+	}
 }
 
 func findKeywordUsingSplit(data string, keyword string) string {
-   arr := strings.Split(data, "\n")
+	arr := strings.Split(data, "\n")
 
-   for i, n := range arr {
-       if strings.Index(n,keyword) != -1 {
-           return arr[i]
-       }
-   }
-   return ""
+	for i, n := range arr {
+		if strings.Index(n, keyword) != -1 {
+			return arr[i]
+		}
+	}
+	return ""
 }
 
 func Find(data []string, keyword string) int {
-   for i, n := range data {
-       if keyword == n {
-           return i
-       }
-   }
-   return -1
+	for i, n := range data {
+		if keyword == n {
+			return i
+		}
+	}
+	return -1
 }
 
 func bytesToString(data []byte) string {
-   return string(data[:])
+	return string(data[:])
 }
 
 func indexOf(slice []string, item string) int {
-   for i, _ := range slice {
-       if slice[i] == item {
-           return i
-       }
-   }
-   return -1
+	for i, _ := range slice {
+		if slice[i] == item {
+			return i
+		}
+	}
+	return -1
 }
